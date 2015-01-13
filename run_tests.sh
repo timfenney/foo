df9fc552f5eb36fdcd02d1c9ee0474c4d99cd3d7
Tim Fenney <kodekata@gmail.com>
2015-01-13 14:19:44 -0500

    Add run_tests.sh to run tests

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+DOTS="..............................................................................\n"
+printf "$DOTS"
+printf ".                              "
+printf "Running tests."
+printf "                                .\n"
+printf "$DOTS"
+
+exit 0
